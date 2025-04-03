15f0793e0aaf00a324483f4a503e90a52b7716c0
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2025-04-03 17:49:01 -0300

    adiciona coluna street à tabela orders

--- db/migrate/20250403023729_add_street_to_order.rb
+++ db/migrate/20250403023729_add_street_to_order.rb
@@ -1,5 +1,4 @@
 class AddStreetToOrder < ActiveRecord::Migration[7.1]
-  # todo: problema ao adicionar a coluna street
   def change
     add_column :orders, :street, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_14_024231) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_03_023729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_14_024231) do
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "street"
     t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
